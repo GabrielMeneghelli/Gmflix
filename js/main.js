fd8cd2bef8e0e2c4150bcabe9e788fd11745aa6d
blessedMene <meneghelli.w7zin@gmail.com>
2025-03-25 11:13:21 -0300

    integração com API MovieDB, alteração das functions para trazer as informações do banco de dados online, atualização do HTML e crianção da categoria TopRated. todo site puxando dados da API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+//API key
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzE2MmJlNDhiODZjNjY5MTkwMzU1NWIxZmM2Y2FmYyIsIm5iZiI6MTc0MjkwNDM0NS4yMywic3ViIjoiNjdlMjljMTlkNzBjNjE1OTAzNzVlNjUzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.dDLO44EdiODg2Ia21nhFxaoTLGGqhhuMBPAI9iQKZVc'
+    }
+}
+
+
 //exibir loading 
 function toggleLoading() {
     let loader = document.querySelector(".loader");
